e329ddac7fa286a508c0a1acd009ff876eda09b1
syuminghuang <syuminghuang@gmail.com>
2020-06-26 12:30:25 +0800

    test

--- src/main.js
+++ src/main.js
@@ -21,12 +21,29 @@ Vue.component("l-marker", LMarker);
 Vue.component("l-popup", LPopup);
 Vue.component("v-marker-cluster", Vue2LeafletMarkerCluster);
 Vue.component("l-control-zoom", LControlZoom);
-axios.defaults.headers.post["Content-Type"] = "application/json;charset=UTF-8";
-axios.defaults.headers["X-Requested-With"] = "XMLHttpRequest";
-axios.defaults.headers["Cache-Control"] = "no-cache";
-axios.defaults.headers["pragma"] = "no-cache";
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-Vue.use(VueAxios, axios);
+// axios.defaults.headers.post["Content-Type"] = "application/json;charset=UTF-8";
+// axios.defaults.headers["X-Requested-With"] = "XMLHttpRequest";
+// axios.defaults.headers["Cache-Control"] = "no-cache";
+// axios.defaults.headers["pragma"] = "no-cache";
+// axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+// http request 欄截
+axios.interceptors.request.use(
+  config => {
+    config.data = JSON.stringify(config.data);
+    config.headers = { // 如果沒有cors的問題則可以都不加
+      "Access-Control-Allow-Origin": '*',
+      "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Authorization,Origin, X-Requested-With, Content-Type, Accept",
+      "Access-Control-Max-Age": "86400"
+    };
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+// Vue.use(VueAxios, axios);
+Vue.prototype.$http = axios
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 
